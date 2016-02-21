216d92e424d4f0dc7ba045c9cd636849b9a1a5c3
tyage <namatyage@gmail.com>
2016-02-21 23:59:16 +0900

    routing

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import koa from 'koa';
+import serve from 'koa-static';
+import Router from 'koa-router';
+
+const apiRouter = Router({
+  prefix: '/api'
+});
+apiRouter
+  .get('/', function *() {
+    this.body = 'Hello World';
+  });
+
+const publicRouter = Router();
+publicRouter
+  .get('/public/*', function *(next) {
+    const [blank, prefix, ...rest] = this.path.split('/');
+    this.path = rest.join('/');
+    yield next;
+  }, serve('public/dist'))
+  .get('/', serve('views'));
 
 const app = koa();
-
-app.use(function *(){
-  this.body = 'Hello World';
-});
-
 app.listen(3000);
+app.use(apiRouter.routes());
+app.use(publicRouter.routes());
