c00e4ce78d9eab0525834ec75721274477639395
tyage <namatyage@gmail.com>
2016-02-22 01:39:39 +0900

    init app

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,1 +1,8 @@
-console.log(1)
+import ReactDOM from 'react-dom';
+import React from 'react';
+import $ from 'jquery';
+import App from './components/App';
+
+$(() => {
+  ReactDOM.render(<App />, $('#app').get(0));
+})
