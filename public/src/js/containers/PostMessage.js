44d56321bd6063bb4920d2ea963a7c2ed88b3fb5
tyage <namatyage@gmail.com>
2016-02-25 20:18:21 +0900

    post form style

--- public/src/js/containers/PostMessage.js
+++ public/src/js/containers/PostMessage.js
@@ -25,11 +25,11 @@ let PostMessage = ({ currentChannel, onSubmit }) => {
       onSubmit(currentChannel, username.value, messageText.value)
       messageText.value = ''
     } } className="post-message-form">
-      <div className="username-form">
+      <div className="username-form form-input">
         <input type="text" placeholder="Username"
           ref={ node => username = node } />
       </div>
-      <div className="message-form">
+      <div className="message-form form-input">
         <input type="text" placeholder="Input message"
           ref={ node => messageText = node } />
       </div>
