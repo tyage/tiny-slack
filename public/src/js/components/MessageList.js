c80373e61c047542d5125515c76c4e449ca83ee8
tyage <namatyage@gmail.com>
2016-03-06 00:39:43 +0900

    auto scroll messages

--- public/src/js/components/MessageList.js
+++ public/src/js/components/MessageList.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react'
 import Message from './Message'
 
-const MessageList = ({ messages }) => {
-  return (
-    <div className="message-list">
-      { messages.map((message) => (
-        <Message
-          message={ message }
-          key={ message._id }
-        />
-      )) }
-    </div>
-  )
+class MessageList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.autoScroll = true
+  }
+  componentDidMount() {
+    const messageList = this.refs.messageList
+    messageList.addEventListener('scroll', (e) => {
+      const rawHeight = getComputedStyle(messageList).height || ''
+      const heightMatch = rawHeight.match(/(\d+)px/)
+      const height = heightMatch ? +heightMatch[1] : 0
+      this.autoScroll = messageList.scrollHeight <= messageList.scrollTop + height
+    })
+  }
+  componentWillUnmount() {
+    messageList.removeEventListener('scroll')
+  }
+  componentDidUpdate() {
+    this.scrollMessages()
+  }
+  scrollMessages() {
+    if (this.autoScroll) {
+      const messageList = this.refs.messageList
+      messageList.scrollTop = messageList.scrollHeight
+    }
+  }
+  render() {
+    const { messages } = this.props
+
+    return (
+      <div className="message-list" ref="messageList">
+        { messages.map((message) => (
+          <Message
+            message={ message }
+            key={ message._id }
+          />
+        )) }
+      </div>
+    )
+  }
 }
 
 export default MessageList
